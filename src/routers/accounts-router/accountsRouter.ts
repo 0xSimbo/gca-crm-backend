0353e6dc27b95770e2891deddfe02b388c3a4a0d
Lironie <tremblay.julien.pro@gmail.com>
2024-05-07 13:14:07 +0900

    added example to route body schema

--- src/routers/accounts-router/accountsRouter.ts
+++ src/routers/accounts-router/accountsRouter.ts
@@ -13,17 +13,21 @@ import { Wallet } from "ethers";
 export const LoginOrSignupQueryBody = t.Object(
   {
     wallet: t.String({
+      example: "0x2e2771032d119fe590FD65061Ad3B366C8e9B7b9",
       minLength: 42,
       maxLength: 42,
     }),
     message: t.String({
+      example: "Sign this message to verify your wallet",
       minLength: 1,
     }),
     signature: t.String({
+      example: "0x" + "a".repeat(130) + "1b", // 132 characters
       minLength: 132,
       maxLength: 132,
     }),
     role: t.String({
+      example: "UNKNOWN",
       enum: accountRoles,
     }),
   },
