19b89711a374f8d15e5521f883f461fcdd8f64ab
Lironie <tremblay.julien.pro@gmail.com>
2024-05-14 07:43:59 +0900

    updated package

--- src/db/queries/applications/findFirstApplicationById.ts
+++ src/db/queries/applications/findFirstApplicationById.ts
@@ -2,7 +2,7 @@ import { eq } from "drizzle-orm";
 import { db } from "../../db";
 import { Applications } from "../../schema";
 
-export const FindFirstApplicationById = async (id: number) => {
+export const FindFirstApplicationById = async (id: string) => {
   const application = await db.query.Applications.findFirst({
     where: eq(Applications.id, id),
     with: {
