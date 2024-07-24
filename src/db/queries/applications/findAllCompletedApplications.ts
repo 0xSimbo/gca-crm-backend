447c0c02dfc1cfd573cb0b8ebf197c4a9b0a6b86
Lironie <tremblay.julien.pro@gmail.com>
2024-07-24 16:40:12 -0400

    reversed order

--- src/db/queries/applications/findAllCompletedApplications.ts
+++ src/db/queries/applications/findAllCompletedApplications.ts
@@ -85,5 +85,6 @@ export const findAllCompletedApplications = async (withDocuments?: boolean) => {
       (a, b) =>
         b.farm!!.auditCompleteDate.getTime() -
         a.farm!!.auditCompleteDate.getTime()
-    );
+    )
+    .reverse();
 };
