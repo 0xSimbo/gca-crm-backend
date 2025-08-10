6c7dbf3caa3d6bc1b98bea386f6c1cba81a724b2
Lironie <tremblay.julien.pro@gmail.com>
2025-08-10 11:15:54 -0400

    return auditFees in get by-application-id

--- src/routers/applications-router/publicRoutes.ts
+++ src/routers/applications-router/publicRoutes.ts
@@ -173,6 +173,7 @@ export const publicApplicationsRoutes = new Elysia()
         } = application;
         return {
           finalProtocolFee,
+          auditFees,
           status,
           currentStep,
           isCancelled,
